22b50063aa8ca0ef1800951c394498da0c8e0c1a
kyumin0411 <rbalsgood@gmail.com>
2022-04-17 16:18:27 +0900

    [dev] swagger 및 port 8081로 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
+import 'dotenv/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as pakage from '../package.json';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  const documentBuilder = new DocumentBuilder()
+    .setTitle(pakage.name)
+    .setDescription(pakage.description)
+    .setVersion(pakage.version)
+    .build();
+
+  const document = SwaggerModule.createDocument(app, documentBuilder);
+
+  SwaggerModule.setup('swagger', app, document);
+
+  await app.listen(process.env.APP_PORT);
 }
 bootstrap();
