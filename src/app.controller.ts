96432495a0febad73392ef967332c5d824847236
kyumin0411 <rbalsgood@gmail.com>
2022-04-17 16:18:48 +0900

    [dev] api controller 생성

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Query, Res, HttpStatus } from '@nestjs/common';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { Response } from 'express';
 import { AppService } from './app.service';
+import * as DTO from 'src/dto/dto';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/parse')
+  @ApiOperation({ summary: '코로나 19 감염 데이터 조회' })
+  @ApiResponse({
+    type: DTO.GetCovidDataResDTO,
+    status: HttpStatus.OK,
+  })
+  // @ApiResponse({
+  //   type: DTO.GetCovidDataResDTO,
+  //   status: HttpStatus.OK,
+  //   description: '',
+  // })
+  async getParsedCovidData(
+    @Res() res: Response,
+    @Query() query: DTO.GetCovidDataReqQueryDTO,
+  ) {
+    const result = await this.appService.getParsedCovidData(query);
+    res.status(result.code).json();
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
+import * as DTO from 'src/dto/dto';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getParsedCovidData(query: DTO.GetCovidDataReqQueryDTO) {
+    const result = new DTO.GetCovidDataResDTO();
+
+    result.code = HttpStatus.OK;
+    result.message = '데이터 조회 성공';
+    return result;
   }
 }
